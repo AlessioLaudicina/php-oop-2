f444f83ba3373f7c3000dd891613bf77a797747d
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-05 15:26:58 +0200

    creazione category

---
+++ Database/db.php
@@ -0,0 +1,6 @@
+<?php
+
+$categories = [
+  'cane' => new Category('Cane','<i class="fa-solid fa-dog"></i>'),
+  'gatto' => new Category('Gatto','<i class="fa-solid fa-cat"></i>'),
+];
--- index.php
+++ index.php
@@ -7,5 +7,33 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
 ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
 -->
 
+<?php 
+require_once __DIR__ . '/Models/Category.php';
+require __DIR__ . '/Database/db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Animal Shop</title>
+</head>
+<body>
+<header class="bg-dark text-white p-5 text-center"> 
+    <h1>Animal Shop</h1>  
+  </header>
+
+    
+</body>
+</html>
+
 
 
